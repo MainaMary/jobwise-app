14c63961b4523db0cb29ad30d09e2352d1701cde
Mary <marieshix98@gmail.com>
2024-01-06 23:56:40 +0300

    feat: initialize ts config file

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app: Express = express();
+const port = process.env || 5000;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Typescript server");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
