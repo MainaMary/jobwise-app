3f9b98eb77d19d5c1662653cc53c670783852145
Mary <marieshix98@gmail.com>
2024-01-25 23:56:24 +0300

    configure morgan with the dev format

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
 const app = express();
-const port = process.env || 5000;
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
 app.get("/", (req, res) => {
   console.log("Server working");
 });
