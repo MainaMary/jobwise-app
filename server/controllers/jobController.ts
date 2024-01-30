18b0245298ec293eb577fa17ba5c45c0f38240f1
Mary <marieshix98@gmail.com>
2024-01-30 23:49:05 +0300

    define CRUD operations

--- server/controllers/jobController.ts
+++ server/controllers/jobController.ts
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import { Request, Response } from "express";
+
 let jobs = [
   {
     id: nanoid(),
@@ -12,6 +13,52 @@ let jobs = [
     title: "Backend",
   },
 ];
-export const getAllJobs = async (req: Request, res: Response) => {
+const getAllJobs = async (req: Request, res: Response) => {
   res.status(200).json({ data: jobs });
 };
+const createJob = async (req: Request, res: Response) => {
+  const { company, title } = req.body;
+  if (!company || !title) {
+    res.status(400).json({ msg: "Please provide all details" });
+    return;
+  }
+  const id = nanoid(10);
+  const job = { id, company, title };
+  jobs.push(job);
+  res.status(201).json({ job });
+};
+const getJob = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with ${id} found` });
+  }
+  res.status(200).json({ job });
+};
+const updateJob = async (req: Request, res: Response) => {
+  const { company, title } = req.body;
+  const { id } = req.params;
+  if (!company || !title) {
+    res.status(404).json({ msg: "Please provide all details" });
+    return;
+  }
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with ${id} found` });
+  }
+  job.company = company;
+  job.title = title;
+  res.status(200).json({ job });
+};
+const deleteJob = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    res.status(404).json({ msg: `No job with ${id} found` });
+  }
+
+  const filteredJobs = jobs.filter((job) => job.id !== id);
+  jobs = filteredJobs;
+  res.status(200).json({ job });
+};
+export { getAllJobs, createJob, getJob, updateJob, deleteJob };
